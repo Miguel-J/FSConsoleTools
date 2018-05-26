124791e82cba6710aa40bc8b1c22529f0938cf5c
Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
2018-05-26 10:20:30 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# FSConsoleTools for FacturaScripts 2018
+
+*Some useful console tools to simplify dev work.*
+
+## Installation
+
+Inside your FacturaScripts run this:
+```
+composer require --dev facturascripts_utils/fs_console_tools:dev-master
+./vendor/bin/order-xml-tables
+```
+
+## Options
+
+- **bin/order-xml-tables:** *This is a little utility to reorder XML table items.*
---
+++ src/Console/Command/OrderJsonFiles.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of FSConsoleTools
+ * Copyright (C) 2018 Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace FacturaScriptsUtils\Console\Command;
+
+use FacturaScriptsUtils\Console\ConsoleAbstract;
+
+/**
+ * Class OrderJsonFiles
+ *
+ * @author Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
+ */
+class OrderJsonFiles extends ConsoleAbstract
+{
+
+    /**
+     * Start point to run the command.
+     *
+     * @param array $params
+     *
+     * @return int
+     */
+    public function run(...$params): int
+    {
+        // TODO: Implement run() method.
+    }
+
+    /**
+     * Return description about this class.
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        // TODO: Implement getDescription() method.
+    }
+
+    /**
+     * Print help information to the user.
+     */
+    public function showHelp()
+    {
+        // TODO: Implement showHelp() method.
+    }
+}
